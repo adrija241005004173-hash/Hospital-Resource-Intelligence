1bcb9a2b7c44a9cdfc417d94a1d22e62b687439c
adrija241005004173-hash <adrija241005004173@technoindiaeducation.com>
2026-09-15 21:37:38 +0530

    Fix Aiven MySQL SSL connection

--- db_connection.py
+++ db_connection.py
@@ -4,8 +4,10 @@ import streamlit as st
 def create_connection():
     connection = mysql.connector.connect(
         host=st.secrets["mysql"]["host"],
+        port=st.secrets["mysql"]["port"],
         user=st.secrets["mysql"]["user"],
         password=st.secrets["mysql"]["password"],
-        database=st.secrets["mysql"]["database"]
+        database=st.secrets["mysql"]["database"],
+        ssl_verify_cert=False
     )
-    return connection 
+    return connection
